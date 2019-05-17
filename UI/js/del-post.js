0783ac3cdb1acf0a9dcb0b32df2725b184d36b1a
Omowunmi Adeniran <omowunmi.whitney@gmail.com>
2019-05-17 14:57:11 +0200

    feature(admin-delete post-js): ensure function is working Make changes to functions to ensure that pop up is showing and can be closed [Finish #165909458]

--- UI/js/del-post.js
+++ UI/js/del-post.js
@@ -1,4 +1,12 @@
-document.getElementById(del-post-btn).addEventListener("click", () =>{
+document.getElementById('del-post-btn').addEventListener('click', () =>{
     document.querySelector('.del-modal').style.display = 'flex';
 });
+
+const btnArray = document.getElementsByClassName('cancel-btn');
+
+for(let i = 0; i<btnArray.length; i++){
+    btnArray[i].addEventListener("click", () =>{
+        document.querySelector('.del-modal').style.display = 'none';
+    });
+}    
 
