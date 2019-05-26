4fe21208916ac0dbd8d10ab9caab9a4607168544
Omowunmi Adeniran <omowunmi.whitney@gmail.com>
2019-05-26 17:41:15 +0200

    feature(sign-up-API-model): add method on model to store new user > Create new model to store user. Methods in model are to perform CR UD operations > [Start 166257625]

---
+++ src/models/User.js
@@ -0,0 +1,44 @@
+import moment from 'moment';
+import uuid from 'uuid';
+
+class User {
+    constructor() {
+        if (typeof this.obj !== 'undefined') {
+            return this.obj;
+        }
+        this.users = {};
+        this.obj = this;
+        return this.obj;
+    }
+
+    create(data) {
+        const id = uuid.v4();
+        this.users[id] = {
+            email: data.email,
+            firstName: data.firstname,
+            lastName: data.lastName,
+            password: data.password,
+            address: data.address,
+            is_admin: data.is_admin,
+            createdOn: moment.now()
+        }
+    }
+
+    update(data) {
+
+    }
+
+    getAll() {
+
+    }
+
+    getUser(user_id) {
+
+    }
+
+    delete() {
+
+    }
+}
+
+export default User;
